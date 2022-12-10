199051ba341c7abec650e517f2e6d725d3082116
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-12-10 15:48:51 +0900

    fix context-baseUrl

---
+++ app/electron/context.ts
@@ -0,0 +1,9 @@
+import { app } from "electron";
+
+const baseUrl = app.isPackaged
+    ? `file://${__dirname}/html/index.html`
+    : "http://localhost:5173";
+
+export {
+    baseUrl,
+};
--- app/electron/rendererWindow.ts
+++ app/electron/rendererWindow.ts
@@ -1,30 +1,31 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
+import { baseUrl } from "./context";
 
 
 let rendererWindow: BrowserWindow;
 // 画面生成
 const createRendererWindow = () => {
-  rendererWindow = new BrowserWindow({
-    width: 640,
-    height: 360,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-      nodeIntegration: false,
-      contextIsolation: true,
-      backgroundThrottling: false,
-    },
-  });
-  rendererWindow.removeMenu();
-  void rendererWindow.loadURL('../index.html');
+    rendererWindow = new BrowserWindow({
+        width: 640,
+        height: 360,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+            nodeIntegration: false,
+            contextIsolation: true,
+            backgroundThrottling: false,
+        },
+    });
+    rendererWindow.removeMenu();
+    void rendererWindow.loadURL(`${baseUrl}?renderer`);
 
-  if (!app.isPackaged) {
-    rendererWindow.webContents.openDevTools();
-  }
+    if (!app.isPackaged) {
+        rendererWindow.webContents.openDevTools();
+    }
 };
 // レンダラーに送信
 // const sendMessageToRenderer = (value: apiResponsesToRenderer) => {
 //   rendererWindow.webContents.send("response", { ...value, target: "renderer" });
 // };
 
-export { createRendererWindow,};
+export { createRendererWindow, };
