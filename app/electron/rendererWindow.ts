9240d7872a22b8b368d20e3f3b7c4a44e8616492
檸檬茶 <green.c.lemon@gmail.com>
2022-12-11 14:13:38 +0900

    add: output xml

--- app/electron/rendererWindow.ts
+++ app/electron/rendererWindow.ts
@@ -4,6 +4,8 @@ import * as path from "path";
 import { baseUrl } from "./context";
 import * as sudo from "sudo-prompt";
 import { exec } from "child_process";
+import fs from "fs-extra";
+import { color } from "./definitions/color";
 
 let rendererWindow: BrowserWindow;
 
@@ -88,6 +90,46 @@ ipcMain.handle("update_prompt", (event, data: string) => {
 });
 
 ipcMain.handle("apply_tprofile", (event, data: TProfile) => {
+  let caretType = 0;
+  if (data.caret.endsWith("block")) {
+    caretType = 0;
+  }
+  if (data.caret.endsWith("underline")) {
+    caretType = 1;
+  }
+  if (data.caret.endsWith("vertical")) {
+    caretType = 2;
+  }
+
+  let caretBlink = data.caret.startsWith("blink-") ? 1 : 0;
+
+  // XMLを生成
+  const xml = `\
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <key>name</key>
+  <string>${data.name}</string>
+  <key>TextColor</key>
+  <data>${color[data.color.screenText.color]}</data>
+  <key>TextBoldColor</key>
+  <data>${color[data.color.boldText.color]}</data>
+	<key>BackgroundColor</key>
+  <data>${color[data.color.screenBg.color]}</data>
+	<key>SelectionColor</key>
+  <data>${color[data.color.selectedText.color]}</data>
+	<key>CursorType</key>
+  <string>${caretType}</string>
+	<key>CursorBlink</key>
+  <string>${caretBlink}</string>
+</plist>
+  `;
+
+  // XMLを書き出し
+  fs.writeFile(`./electron/data/${data.name}.terminal`, xml, (error) => {
+    console.log(error);
+  });
+
   const prompt = data.prompt.reduce((pv, value) => {
     return pv + value.value + " ";
   }, "");
